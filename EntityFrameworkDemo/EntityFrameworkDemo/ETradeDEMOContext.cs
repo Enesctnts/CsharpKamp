ab7007741cd30dc2ffac78d3dcb6d92985d184c0
Enes Çetintaş <enesctnts@gmail.com>
2022-01-29 19:14:59 +0300

    EntityFramework
    EntityFramework

---
+++ EntityFrameworkDemo/EntityFrameworkDemo/ETradeDEMOContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    public class ETradeDEMOContext : DbContext
+    {
+        public DbSet<Product> Product_DEMO { get; set; }
+        
+    }
+}
---
+++ EntityFrameworkDemo/EntityFrameworkDemo/ProductDal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    class ProductDal
+    {
+        public List<Product> GetAll()
+        {
+            using (ETradeDEMOContext context = new ETradeDEMOContext())
+            {
+                return context.Product_DEMO.ToList();
+            }
+        }
+        public void Add(Product product)
+        {
+            using (ETradeDEMOContext context = new ETradeDEMOContext())
+            {
+                context.Product_DEMO.Add(product);
+                context.SaveChanges();
+
+            }
+
+        }
+        public void Update(Product product)
+        {
+            using (ETradeDEMOContext context = new ETradeDEMOContext())
+            {
+                var entity = context.Entry(product);
+                entity.State = EntityState.Modified;
+                context.SaveChanges();
+
+            }
+
+        }
+        public void Delete(Product product)
+        {
+            using (ETradeDEMOContext context = new ETradeDEMOContext())
+            {
+                var entity = context.Entry(product);
+                entity.State = EntityState.Deleted;
+                context.SaveChanges();
+
+            }
+
+        }
+
+
+    }
+}
